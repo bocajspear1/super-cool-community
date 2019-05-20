a788e6a26751c3c90bec2d598f280dcbde24f3f8
Jacob Hartman <junyunharold1@gmail.com>
2019-05-20 10:54:04 -0400

    Added more SQL injection stuff...

--- page.php
+++ page.php
@@ -19,49 +19,58 @@
 
                 $query = "SELECT * FROM users WHERE username='" . $username . "'";
 
-                $result = $mysqli->query($query);
+                $resp = $mysqli->multi_query($query);
+                
 
-                if (!$result) {
+                if (!$resp) {
                     echo "<div class='error'>Failed to query: (" . $mysqli->errno . ") " . $mysqli->error . "</div>";
-                } else if ($result->num_rows == 0) {
-                    echo "<div class='error'>The user $username does not exist</div>";
                 } else {
-                    $user_data = $result->fetch_assoc();
-                    echo "<div id='profile-top'>";
-                    echo "<h1>" . $user_data['fullname'] . "</h1>";
-                    echo "<h3>" . $user_data['username'] . "</h2>";
-                    echo "<div id='description'>" . $user_data['description'] . "</div>";
-                    echo "</div><hr>";
-                    echo "<div id='posts'>";
-                    echo "<h3>Posts</h3>";
-
-                    $query = "SELECT * FROM posts WHERE userid='" . $user_data['userid'] . "'";
-                    $posts = $mysqli->query($query);
-                    if ($posts&&$posts->num_rows > 0) {
-                        while ($post = $posts->fetch_assoc()) {
-                            echo "<div class='post'>";
-                            echo "<h4>" . $post['title'] . "</h4>";
-                            echo "<p>" . $post['text'] . "</p>";
-                            echo "</div>";
+                    do {
+                        $result = $mysqli->store_result();
+                        if ($result) {
+                            if ($result->num_rows == 0) {
+                                echo "<div class='error'>The user $username does not exist</div>";
+                            } else {
+                                $user_data = $result->fetch_assoc();
+                                echo "<div id='profile-top'>";
+                                echo "<h1>" . $user_data['fullname'] . "</h1>";
+                                echo "<h3>" . $user_data['username'] . "</h2>";
+                                echo "<div id='description'>" . $user_data['description'] . "</div>";
+                                echo "</div><hr>";
+                                echo "<div id='posts'>";
+                                echo "<h3>Posts</h3>";
+            
+                                $query = "SELECT * FROM posts WHERE userid='" . $user_data['userid'] . "'";
+                                $posts = $mysqli->query($query);
+                                if ($posts&&$posts->num_rows > 0) {
+                                    while ($post = $posts->fetch_assoc()) {
+                                        echo "<div class='post'>";
+                                        echo "<h4>" . $post['title'] . "</h4>";
+                                        echo "<p>" . $post['text'] . "</p>";
+                                        echo "</div>";
+                                    }
+                                    
+                                }
+            
+                                echo "</div>";
+                                echo "<div id='uploads'>";
+                                echo "<h3>Uploads</h3>";
+                                $query = "SELECT * FROM uploads WHERE userid='" . $user_data['userid'] . "'";
+                                $uploads = $mysqli->query($query);
+                                if ($uploads&&$uploads->num_rows > 0) {
+                                    while ($upload = $uploads->fetch_assoc()) {
+                                        echo "<div class='upload'>";
+                                        echo "<h4><a href='" . $config->upload_dir . "/" . $upload['filename'] . "'>" . $upload['name'] . "</a></h4>";
+                                        echo "<p>" . $upload['description'] . "</p>";
+                                        echo "</div>";
+                                    }
+                                    
+                                }
+                                echo "</div>";
+                            }
+                            $result->free();
                         }
-                        
-                    }
-
-                    echo "</div>";
-                    echo "<div id='uploads'>";
-                    echo "<h3>Uploads</h3>";
-                    $query = "SELECT * FROM uploads WHERE userid='" . $user_data['userid'] . "'";
-                    $uploads = $mysqli->query($query);
-                    if ($uploads&&$uploads->num_rows > 0) {
-                        while ($upload = $uploads->fetch_assoc()) {
-                            echo "<div class='upload'>";
-                            echo "<h4><a href='" . $config->upload_dir . "/" . $upload['filename'] . "'>" . $upload['name'] . "</a></h4>";
-                            echo "<p>" . $upload['description'] . "</p>";
-                            echo "</div>";
-                        }
-                        
-                    }
-                    echo "</div>";
+                    } while ($mysqli->next_result());
                 }
 
             } else {
