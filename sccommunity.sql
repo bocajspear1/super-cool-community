f388421168453c30f65e16305d4f45e57fa8b6dd
Jacob Hartman <junyunharold1@gmail.com>
2022-10-25 17:51:16 -0400

    Updated SQL file

--- sccommunity.sql
+++ sccommunity.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7deb1
+-- version 4.6.6deb5ubuntu0.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 06, 2021 at 12:37 AM
--- Server version: 8.0.23-0ubuntu0.20.10.1
--- PHP Version: 7.4.9
+-- Generation Time: Oct 25, 2022 at 09:50 PM
+-- Server version: 10.1.48-MariaDB-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,12 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `commentid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `postid` int(11) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`commentid`, `userid`, `postid`, `comment`) VALUES
+(1, 2, 1, 'Did you go to space?'),
+(2, 1, 1, 'Yes I did'),
+(3, 2, 1, 'Was it cold?');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
 CREATE TABLE `posts` (
-  `postid` int NOT NULL,
-  `userid` int NOT NULL,
+  `postid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
   `title` varchar(200) NOT NULL,
   `text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -40,7 +60,8 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`postid`, `userid`, `title`, `text`) VALUES
-(1, 1, 'Ground control to Major Tom', 'Take your protein pills and put your helmet on');
+(1, 1, 'Ground control to Major Tom', 'Take your protein pills and put your helmet on'),
+(2, 2, 'Looking for Black Holes', 'How do I find them?');
 
 -- --------------------------------------------------------
 
@@ -49,12 +70,19 @@ INSERT INTO `posts` (`postid`, `userid`, `title`, `text`) VALUES
 --
 
 CREATE TABLE `uploads` (
-  `uploadid` int NOT NULL,
-  `userid` int NOT NULL,
+  `uploadid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `description` text NOT NULL,
   `filename` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `uploads`
+--
+
+INSERT INTO `uploads` (`uploadid`, `userid`, `name`, `description`, `filename`) VALUES
+(1, 2, 'Blackhole List', 'Blackholes I want to see!', 'bobby.Blackhole List.txt');
 
 -- --------------------------------------------------------
 
@@ -63,7 +91,7 @@ CREATE TABLE `uploads` (
 --
 
 CREATE TABLE `users` (
-  `userid` int NOT NULL,
+  `userid` int(11) NOT NULL,
   `username` varchar(75) NOT NULL,
   `fullname` varchar(100) NOT NULL,
   `password` varchar(40) NOT NULL,
@@ -76,11 +104,17 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userid`, `username`, `fullname`, `password`, `description`) VALUES
 (1, 'tom', 'Major Tom', 'spaceiscold', 'I\'m stepping through the door, and I\'m floating in a most peculiar way'),
-(2, 'bobby', 'Bobby Tables', 'hackme', 'My name is Bobby Tables. Black holes are cool');
+(2, 'bobby', 'Bobby Tables', 'hackme', 'My name is Bobby Tables. Black holes are cool!');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`commentid`);
 
 --
 -- Indexes for table `posts`
@@ -105,24 +139,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `postid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `postid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `uploads`
 --
 ALTER TABLE `uploads`
-  MODIFY `uploadid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
+  MODIFY `uploadid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
