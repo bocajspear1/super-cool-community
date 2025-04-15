8fd05dc09928a199300b2e8781c7db4fd146b9b3
Jacob Hartman <junyunharold1@gmail.com>
2025-04-15 17:34:04 -0400

    Spelling fix

--- app/pages/rules.php
+++ app/pages/rules.php
@@ -2,6 +2,6 @@
     <ol>
         <li>Be nice!</li>
         <li>Talk about space</li>
-        <li>Don't take about asteroids destroying earth</li>
+        <li>Don't talk about asteroids destroying earth</li>
     </ol>
 </p>
